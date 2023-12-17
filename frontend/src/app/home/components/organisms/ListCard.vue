78e22e75c0f7aa41a5fe48a8c41c773571d2104d
Luis Miguel Ventura E <luisigsystem@gmail.com>
2023-12-16 19:49:44 -0500

    grid list card

--- frontend/src/app/home/components/organisms/ListCard.vue
+++ frontend/src/app/home/components/organisms/ListCard.vue
@@ -1,9 +1,29 @@
 <template>
   <div class="list-card">
-    <poke-card
-      imageUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/152.svg"
-      name="Chikorita"
-    />
+    <div class="list-card__item">
+      <poke-card
+        imageUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/152.svg"
+        name="Chikorita"
+      />
+    </div>
+    <div class="list-card__item">
+      <poke-card
+        imageUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/152.svg"
+        name="Chikorita"
+      />
+    </div>
+    <div class="list-card__item">
+      <poke-card
+        imageUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/152.svg"
+        name="Chikorita"
+      />
+    </div>
+    <div class="list-card__item">
+      <poke-card
+        imageUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/152.svg"
+        name="Chikorita"
+      />
+    </div>
   </div>
 </template>
 
@@ -19,4 +39,12 @@ export default {
 <style lang="sass" scoped>
 .list-card
   margin: 28px 0
+  display: grid
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr))
+  grid-gap: 10px
+  width: 100%
+  &__item
+    display: flex
+    justify-content: space-evenly
+    align-items: center
 </style>
